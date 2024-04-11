5d4e573d7764462c15834ee83a84641896a8ded8
rafaelfsobral <faelsobral1@gmail.com>
2024-04-10 22:35:50 -0300

    Desenvolvimento dos repositórios e controladores

--- src/controllers/ClienteController.java
+++ src/controllers/ClienteController.java
@@ -1,17 +1,18 @@
 package controllers;
 
+import java.util.List;
 import java.util.Scanner;
 
 import entities.Cliente;
 import repositories.ClienteRepository;
 
-public class ClienteController {
+public class ClienteController { // Vamos montar as interações com o usuário do sistema
 
 	// vai interagir mais com o usuário através do Scanner, do Console
 	// O nosso controller quando chamar o método cadastrarCliente, vai imprimir cadastro de clientes, vai pegar os dados que o usuário fornecer, 
 	// quando preencher os dados, vai acessar o ClienteRepository e vai passar para o Repository o objeto e vai gravar ele
 	public void cadastrarCliente() {
-		System.out.println("\n** CADASTRO DE CLIENTES **\n");
+		System.out.println("\n*** CADASTRO DE CLIENTES ***\n");
 		Scanner scanner = new Scanner(System.in); // ler os dados que o usuário vai preencher e vamos guardar no objeto
 		Cliente cliente = new Cliente();
 		
@@ -38,4 +39,83 @@ public class ClienteController {
 		}
 	}
 	
+	public void atualizarCliente() { // adtualizar dados do cliente
+	
+		System.out.println("\n*** ATUALIZAÇÃO DE CLIENTES ***\n");
+		Scanner scanner = new Scanner(System.in); // ler os dados que o usuário vai preencher e vamos guardar no objeto
+		Cliente cliente = new Cliente();
+		
+		System.out.print("ID DO CLIENTE ............: ");
+		cliente.setIdCliente(Integer.parseInt(scanner.nextLine()));
+
+		System.out.print("NOME DO CLIENTE ..........: ");
+		cliente.setNome(scanner.nextLine());
+		
+		System.out.print("E-MAIL ...................: ");
+		cliente.setEmail(scanner.nextLine());
+		
+		System.out.print("TELEFONE .................: ");
+		cliente.setTelefone(scanner.nextLine());
+		
+		System.out.print("CPF ......................: ");
+		cliente.setCpf(scanner.nextLine());
+	
+		ClienteRepository clienteRepository = new ClienteRepository(); // vamos acessar o repositório
+		
+		try {
+			clienteRepository.update(cliente);
+			System.out.println("\nCLIENTE ATUALIZADO COM SUCESSO!\n");
+		}catch(Exception e) {
+			System.out.println("\nFALHA AO ATUALIZAR CLIENTE!\n");
+			e.printStackTrace(); // imprimir log de erro
+		}
+		
+	}
+	
+	public void excluirCliente() {
+
+		System.out.println("\n*** EXCLUSÃO DE CLIENTES ***\n");
+		Scanner scanner = new Scanner(System.in); // ler os dados que o usuário vai preencher e vamos guardar no objeto
+		Cliente cliente = new Cliente();
+		
+		System.out.print("ID DO CLIENTE ............: ");
+		cliente.setIdCliente(Integer.parseInt(scanner.nextLine()));
+		
+		ClienteRepository clienteRepository = new ClienteRepository();
+		
+		try {
+			clienteRepository.delete(cliente);
+			System.out.println("\nCLIENTE EXCLUÍDO COM SUCESSO!\n");
+		}catch(Exception e) {
+			System.out.println("\nFALHA AO EXCLUIR CLIENTE!\n");
+			e.printStackTrace(); // imprimir log de erro
+		}
+		
+	}
+	
+	public void consultarClientes() {
+		
+		System.out.println("\n*** CONSULTA DE CLIENTES ***\n");
+		
+		ClienteRepository clienteRepository = new ClienteRepository(); 
+		
+		try {
+		
+			List<Cliente> lista = clienteRepository.findAll(); // consulta os dados de clientes e joga na lista. Essa lista recebe todos os clientes do repositório
+			
+			for(Cliente cliente : lista) { // foreach -> para cada cliente que ele lê da lista, ele imprime os dados
+				// vou imprimir os dados dos clientes
+				System.out.println("ID DO CLIENTE .........: " + cliente.getIdCliente());
+				System.out.println("NOME ..................: " + cliente.getNome());
+				System.out.println("E-MAIL ................: " + cliente.getEmail());
+				System.out.println("TELEFONE ..............: " + cliente.getTelefone());
+				System.out.println("CPF ...................: " + cliente.getCpf());
+				System.out.println("...");
+			}
+			
+		}catch(Exception e) {
+			System.out.println("\nFALHA AO CONSULTAR CLIENTES!\n");
+			e.printStackTrace();
+		}
+	}
 }
--- src/repositories/ClienteRepository.java
+++ src/repositories/ClienteRepository.java
@@ -1,7 +1,11 @@
 package repositories;
 
+import java.beans.Statement;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 import entities.Cliente;
 import factories.ConnectionFactory;
@@ -28,10 +32,85 @@ public class ClienteRepository {
 		statement.setString(4, cliente.getCpf());
 		statement.execute(); // executar o comando
 		
-		statement.close(); // fechar a conexão, liberar a conexão
-		
+		connection.close(); // fechar a conexão, liberar a conexão
 		
 	}
 	
+	// método para receber e atualizar um cliente no banco de dados
+	public void update(Cliente cliente) throws Exception{
+		
+		// abrindo conexão com banco de dados
+		ConnectionFactory connectionFactory = new ConnectionFactory(); // conecta no PostGre
+		Connection connection = connectionFactory.getConnection(); // retorna a conexão
+		
+		// escrever um comando em linguagem SQL para atualizar quaisquer dados de um cliente no banco de dados
+		PreparedStatement statement = connection.prepareStatement("update cliente set nome=?, email=?, telefone=?, cpf=? where idcliente=?");
+		statement.setString(1, cliente.getNome());
+		statement.setString(2, cliente.getEmail());
+		statement.setString(3, cliente.getTelefone());
+		statement.setString(4, cliente.getCpf());
+		statement.setInt(5, cliente.getIdCliente());
+		statement.execute();
+		
+		// fechando a conexão
+		connection.close();
+	}
+	
+	// método para receber e excluir um cliente no banco de dados
+	public void delete(Cliente cliente) throws Exception{
+		
+		// abrindo conexão com banco de dados
+		ConnectionFactory connectionFactory = new ConnectionFactory();
+		Connection connection = connectionFactory.getConnection();
+		
+		// escrevendo um comando em linguagem SQL para excluir um cliente no banco de dados
+		PreparedStatement statement = connection.prepareStatement("delete from cliente where idcliente=?");
+		statement.setInt(1, cliente.getIdCliente());
+		statement.execute();
+		
+		// fechando a conexão
+		connection.close();
+			
+	}
+	
+	// método para consultar todos os clientes cadastrados no banco de dados
+	public List<Cliente> findAll() throws Exception{
+		
+		// abrindo conexão com banco de dados
+		ConnectionFactory connectionFactory = new ConnectionFactory();
+		Connection connection = connectionFactory.getConnection();
+		
+		// escrevendo um comando em linguagem SQL para consultar todos os clientes
+		PreparedStatement statement = connection.prepareStatement("select * from cliente");
+		// ResultSet é uma classe para poder capturar todos os clientes que estamos lendo no banco de dados
+		// executeQuery() -> Esse comando está executando a query, está capturando todos os registros do banco de dados e armazenando no resultset
+		ResultSet resultset = statement.executeQuery();
+		
+		// declarando uma lista de clientes
+		List<Cliente> lista = new ArrayList<Cliente>();
+		
+		// percorrer cada registro obtido do banco de dados e armazenar na lista. Estou lendo cliente por cliente, registro a registro.
+		while(resultset.next()) {  // next() é um cursor, é como se fosse um ponteiro que está lendo cada cliente que trouxemos do banco de dados
+			// enquanto tiver registros, leia
+			
+			// criando objeto cliente
+			Cliente cliente = new Cliente();
+			
+			// capturar os campos do banco de dados
+			cliente.setIdCliente(resultset.getInt("idcliente"));
+			cliente.setNome(resultset.getString("nome"));
+			cliente.setEmail(resultset.getString("email"));
+			cliente.setTelefone(resultset.getString("telefone"));
+			cliente.setCpf(resultset.getString("cpf"));
+			
+			// adicionar cada objeto cliente dentro da lista
+			lista.add(cliente);
+		}
+		
+		// fechando conexão
+		connection.close();
+		
+		return lista; // retornando a listagem de clientes
+	}
 	
 }
